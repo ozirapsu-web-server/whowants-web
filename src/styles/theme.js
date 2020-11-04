86f71fcaa703a308bfa2ae55aad999bb59d0a34c
haerang <gofkdvjvl@naver.com>
2020-11-05 03:42:43 +0900

    feat: 기본 환경 세팅 및 글로벌 스타일 적용

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    lightBlue: "#7DC1E0",
+    deepPurple: "#5f27cd",
+  },
+  size: {
+    sm: "10px",
+    smd: "12px",
+    mmd: "14px",
+    md: "20px",
+    lg: "32px",
+  },
+};
+
+export default theme;
