00758c523b96f2e11d272479d5570a26d9110cb5
haerang <gofkdvjvl@naver.com>
2020-11-05 04:31:05 +0900

    feat: carousel view 이미지 적용하기

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Logo from "components/Logo";
+import styled from "styled-components";
+import study1 from "images/study1.jpg";
+
+const CarouselWrapper = styled.div`
+  min-height: 350px;
+  width: 100%;
+  background: url(${study1}) no-repeat center center;
+  background-size: cover;
+`;
 
 const Carousel = () => {
   return (
-    <div>
+    <CarouselWrapper>
       <Logo />
-    </div>
+    </CarouselWrapper>
   );
 };
 
