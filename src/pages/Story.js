cd5add72593042bfdf3b536bef9bded18c7a1c37
haerang <gofkdvjvl@naver.com>
2020-11-08 17:49:37 +0900

    feat: 플로팅 버튼 컴포넌트 UI 구현

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -4,11 +4,13 @@ import SummaryContainer from "containers/SummaryContainer";
 import DonationContainer from "containers/DonationContainer";
 import TabsContainer from "containers/TabsContainer";
 import OrganizerContainer from "containers/OrganizerContainer";
+import FloatingContainer from "containers/FloatingContainer";
 import styled from "styled-components";
 
 const StoryWrapper = styled.section`
   width: 100%;
   height: 100%;
+  margin-bottom: 125px;
 `;
 
 const Story = React.memo(() => {
@@ -19,6 +21,7 @@ const Story = React.memo(() => {
       <DonationContainer />
       <TabsContainer />
       <OrganizerContainer />
+      <FloatingContainer />
     </StoryWrapper>
   );
 });
