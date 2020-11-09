b53cb3edd293eef4106582f6a833e1995dfb941c
haerang <gofkdvjvl@naver.com>
2020-11-09 20:51:59 +0900

    feat: comment 안내문 UI 구현

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { numberWithCommas } from "utils/util";
 const CommentItem = styled.div`
   width: 100%;
   min-height: 90px;
@@ -9,7 +9,8 @@ const CommentItem = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  padding: 10px;
+  padding: 10px 20px;
+  margin-bottom: 10px;
 `;
 
 const Name = styled.div`
@@ -35,7 +36,7 @@ const Comment = ({ name, amount, comment }) => {
     <CommentItem>
       <Name>{name}</Name>
       <AmountContainer>
-        <div>{amount}</div>
+        <div>{numberWithCommas(amount)}</div>
         <div>원 후원</div>
       </AmountContainer>
       <CommentSection>{comment}</CommentSection>
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -7,9 +7,30 @@ const CommentWrapper = styled.section`
   padding: 20px;
 `;
 
-const CommentList = () => {
+const Notice = styled.div`
+  height: 60px;
+  padding: 10px 5px;
+  width: 100%;
+  background: ${(props) => props.theme.color.mint};
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+const NumberOfSupporter = styled.div`
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const CommentList = ({ numOfSupporter }) => {
   return (
     <CommentWrapper>
+      <Notice>
+        <NumberOfSupporter>{numOfSupporter}명의 서포터</NumberOfSupporter>
+        <div>가 이 사연을 응원합니다.</div>
+      </Notice>
       <Comment name="최해랑" amount="50000" comment="응원합니다" />
     </CommentWrapper>
   );
