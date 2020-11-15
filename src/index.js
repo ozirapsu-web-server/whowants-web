78baff85167194df01dc10746980dbb7306481d1
haerang <gofkdvjvl@naver.com>
2020-11-15 17:17:36 +0900

    feat: api 경로 변경 후 배포

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import rootReducer from "modules";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "/" : "http://13.209.249.225:3000/";
 
 const store = createStore(
   rootReducer,
