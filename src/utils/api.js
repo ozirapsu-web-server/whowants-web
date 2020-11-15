62329d3ee6ea67408fd17fc052b079487cc53b29
haerang <gofkdvjvl@naver.com>
2020-11-15 19:28:27 +0900

    feat: getAllComments api 연동 완료

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getStory = (idx) => axios.get(`/story/${idx}`);
 
 export const getImgs = (idx) => axios.get(`/story/${idx}/image`);
+
+export const getAllComments = (idx, filter) =>
+  axios.get(`/support/?story_idx=
+${idx}&filter=${filter}`);
