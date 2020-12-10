df83d0c74ce6c2b51fc9c5c319917b23d9f12498
haerang <gofkdvjvl@naver.com>
2020-12-10 20:33:10 +0900

    feat: tab3 중간 모금 참여 문구 UI 구현

--- src/components/Tab3.js
+++ src/components/Tab3.js
@@ -18,7 +18,18 @@ const ParticipateBtn = styled(Btn)`
   line-height: 20px;
 `;
 
-const Supporter = ({ comments, lenOfComments, pageIdx }) => {
+const MiddleNotice = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  font-weight: 300;
+  line-height: 20px;
+  color: ${(props) => props.theme.color.black};
+  margin: 20px 0;
+`;
+
+const Tab3 = ({ comments, lenOfComments, pageIdx }) => {
   const history = useHistory();
   const onClick = useCallback(() => {
     history.push(`/participate/${pageIdx}`);
@@ -37,16 +48,24 @@ const Supporter = ({ comments, lenOfComments, pageIdx }) => {
       {comments &&
         comments.map((comment, idx) => {
           return (
-            <Comment
-              key={`supporters-${idx}`}
-              support_nickname={comment.support_nickname}
-              support_amount={comment.support_amount}
-              support_comment={comment.support_comment}
-            />
+            <>
+              <Comment
+                key={`supporters-${idx}`}
+                support_nickname={comment.support_nickname}
+                support_amount={comment.support_amount}
+                support_comment={comment.support_comment}
+              />
+              {idx === 4 && (
+                <MiddleNotice key={`mid-notice-${idx}`}>
+                  <div>{lenOfComments}명의 서포터즈와 함께해요.</div>
+                  <div> 모금에 참여해보세요.</div>
+                </MiddleNotice>
+              )}
+            </>
           );
         })}
     </CommentWrapper>
   );
 };
 
-export default Supporter;
+export default Tab3;
