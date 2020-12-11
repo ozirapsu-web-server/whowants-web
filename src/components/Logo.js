8d329a97eabde6c0e882d0cd7e8826347bc761d0
haerang <gofkdvjvl@naver.com>
2020-12-11 16:13:30 +0900

    logo 컨테이너 pc화면 border-bottom 속성 추가

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { withRouter, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import logo from "images/logo.svg";
+import { PC_MIN } from "utils/media";
 
 const Header = styled.header`
   display: grid;
@@ -11,7 +12,8 @@ const Header = styled.header`
   position: fixed;
   top: 0;
   left: 0;
-  background: ${props=>props.background==='gray'?props.theme.color.grayB2:'#fff'};
+  background: ${(props) =>
+    props.background === "gray" ? props.theme.color.grayB2 : "#fff"};
   width: 100%;
   height: 55px;
   z-index: 10;
@@ -22,32 +24,35 @@ const Header = styled.header`
   & img:nth-child(2) {
     height: auto;
   }
+  @media only screen and (min-width: ${PC_MIN}px) {
+    border-bottom: 1px solid #c7c7c7;
+  }
 `;
-//  로고 크기 줄이기 위한 사이즈 
-const Img=styled.img`
-  max-height:18.23px;
+//  로고 크기 줄이기 위한 사이즈
+const Img = styled.img`
+  max-height: 18.23px;
 `;
 
 //  후원츠 로고
-const Logo = React.memo(({ left, history,background }) => {
+const Logo = React.memo(({ left, history, background }) => {
   const location = useLocation();
   // 현재 위치에 따라 이동하는 위치
   const moveTo = useCallback(() => {
     if (location.pathname === "/linkPage") {
       history.goBack();
-    } else  {
+    } else {
       history.push("/linkPage");
     }
   }, [history, location]);
 
-  const home=useCallback(()=>{
-    history.push('/');
-  },[history])
+  const home = useCallback(() => {
+    history.push("/");
+  }, [history]);
 
   return (
     <Header background={background}>
       <Img src={left} alt="메뉴바" onClick={moveTo} />
-      <Img src={logo} alt="로고" onClick={home}/>
+      <Img src={logo} alt="로고" onClick={home} />
     </Header>
   );
 });
